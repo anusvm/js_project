431c05039b254bfc6434a6881b6cea4450ddabe4
anusvm <99947691+anusvm@users.noreply.github.com>
2024-06-22 06:21:13 +0000

    add variable file

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 144332
+let accountEmail = "anuanu@gmail.com"
+var accountPassword = "1461235"
+accountCity = "tilouthu"
+let accountState ;
+//accountId =2 not allowed
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity, accountState])
+
+if (condition) {
+    
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
