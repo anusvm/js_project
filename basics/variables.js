131d8511f2e49f052b821ba48df18bcb0eed7e53
anusvm <99947691+anusvm@users.noreply.github.com>
2024-06-23 05:27:34 +0000

    add object

--- basics/variables.js
+++ basics/variables.js
@@ -16,3 +16,22 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//stack memory(Primitive- get the copy of the orginal value) ///Heap Memory(Non-Primitive - grt the refrence from the original value )
+let myYoutubeNmae= "Anupriya"
+
+let anotherName =myYoutubeNmae
+anotherName = "anupriyatilouthu"
+
+console.log(anotherName);
+console.log(myYoutubeNmae);
+
+/*****************Object***************/
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@paytm"
+}
+let userTwo = userOne
+//elements inside the object can be access using dot such userOne.email, userOne.upi........etc
+userOne.email="supriya@gmail.com"
