67961b663acdad56d6651fbee8a3e520900925e7
anusvm <99947691+anusvm@users.noreply.github.com>
2024-06-24 06:16:53 +0000

    date and Time

---
+++ basics/dates.js
@@ -0,0 +1,17 @@
+//dates
+
+let myDate = new Date()
+console.log(myDate); 
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+
+//to declare a specific date
+
+let myCreatedDate1 = new Date(2024, 2, 5)
+let myCreatedDate2= new Date("20-02-2024")
+console.log(myCreatedDate.toDateString());
+
+//`${newDate.getDay()}  and the time`
