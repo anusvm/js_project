59bac17bf85902bdcfe0a33d5f95a3f7019f12a3
anusvm <99947691+anusvm@users.noreply.github.com>
2024-06-24 05:37:22 +0000

    maths and num

---
+++ basics/numandmath.js
@@ -0,0 +1,18 @@
+const score = 400
+const balance = new Number(100)
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toFixed(2));
+const num = 12354.854
+// console.log(num.toPrecision(6));
+// console.log(num.toLocaleString('en-IN'));
+/*************************maths**********************/
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.floor(6.8));
+console.log(Math.random());
+
+const min = 10;
+const max= 20;
+console.log(Math.floor(Math.random()*(max-min +1)) +min)
