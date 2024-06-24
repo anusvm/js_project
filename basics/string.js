cb2a76c6647f12b2726d69e83ac6d76807dc74d8
anusvm <99947691+anusvm@users.noreply.github.com>
2024-06-24 05:01:08 +0000

    do string

---
+++ basics/string.js
@@ -0,0 +1,13 @@
+const name = "anupriya"
+const repoCount = 15
+console.log(name + repoCount +"value") 
+console.log(`Helo my name is ${name} and my repocount is ${repoCount}`);
+const gameName = new String('anupriya')
+
+console.log(gameName[0]);
+//console.log(gameName.__pr__);
+console.log(gameName.startsWith());
+console.log(gameName.toUpperCase());
+
+let myName = "    Anupriy   "
+console.log(myName.trim());
